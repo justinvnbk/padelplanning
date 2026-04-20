2d35e58487c21d737ec2dc7ade6e67725c706df0
Justin Van Beek <justin.vanbeek@student.thomasmore.be>
2026-04-20 15:37:32 +0200

    Created project

---
+++ src/main/java/be/thomasmore/padelplanning/PadelplanningApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.padelplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelplanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadelplanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/padelplanning/PadelplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.padelplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelplanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
