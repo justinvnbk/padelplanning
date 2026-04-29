799d7200297161983772fc5071164f6ab673d0cb
Justin Van Beek <justinvnbk@gmail.com>
2026-04-29 12:36:30 +0200

    Created basic framework to create a new padelDay as admin

--- src/main/java/be/thomasmore/padelplanning/controllers/admin/PlanController.java
+++ src/main/java/be/thomasmore/padelplanning/controllers/admin/PlanController.java
@@ -3,14 +3,17 @@ package be.thomasmore.padelplanning.controllers.admin;
 import be.thomasmore.padelplanning.model.Field;
 import be.thomasmore.padelplanning.model.PadelDay;
 import be.thomasmore.padelplanning.repositories.FieldRepository;
+import be.thomasmore.padelplanning.repositories.PadelDayRepository;
 import be.thomasmore.padelplanning.repositories.PlayerRepository;
 import be.thomasmore.padelplanning.services.CreatePadelDayService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Controller
@@ -19,29 +22,35 @@ public class PlanController {
     private final FieldRepository fieldRepository;
     private final PlayerRepository playerRepository;
     private final CreatePadelDayService createPadelDayService;
+    private final PadelDayRepository padelDayRepository;
 
-    public PlanController(FieldRepository fieldRepository, PlayerRepository playerRepository, CreatePadelDayService createPadelDayService) {
+    public PlanController(FieldRepository fieldRepository, PlayerRepository playerRepository, CreatePadelDayService createPadelDayService, PadelDayRepository padelDayRepository) {
         this.fieldRepository = fieldRepository;
         this.playerRepository = playerRepository;
         this.createPadelDayService = createPadelDayService;
+        this.padelDayRepository = padelDayRepository;
     }
 
     @GetMapping("/plan")
     public String plan(Model model){
-        int timeSlots = 3; //The amount of timeslots available per player
-        List<Field> availableFields = fieldRepository.getAvailable();
-        LocalDateTime dateAndStartTime = LocalDateTime.of(2026,4,25,14,0);
-        int matchDurationInMinutes = 40; //doesn't include the pause in between matches
-
-        //First we need a new padelDay with a list of signedup players.
-        PadelDay padelDay = new PadelDay();
-        padelDay.setDate(dateAndStartTime);
-        padelDay.setSignedUpPlayers(playerRepository.getAll());
-
-        //This will create update padelDay with all linked entities (matches and teams) and save them to the database
-        createPadelDayService.newPadelDayPlanning(padelDay, timeSlots, availableFields, matchDurationInMinutes);
+        PadelDay padelDay = padelDayRepository.getLast();
 
         model.addAttribute("padelDay", padelDay);
         return "admin/plan";
+    }
+
+    @GetMapping("/nieuwplan")
+    public String nieuwplan(Model model){
+        Iterable<Field> fields = fieldRepository.findAll();
+        model.addAttribute("fields", fields);
+        model.addAttribute("padelDay", new PadelDay());
+        return "admin/nieuwplan";
+    }
+
+    @PostMapping("/nieuwplan")
+    public String postNieuwPlan(Model model, PadelDay padelDay){
+        padelDay.setSignedUpPlayers(playerRepository.getAll());
+        padelDayRepository.save(padelDay);
+        return "redirect:/admin/plan";
     }
 }
