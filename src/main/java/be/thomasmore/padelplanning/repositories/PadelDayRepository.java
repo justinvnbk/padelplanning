ea1226f808f6e1887033556788fe6a11f0d82265
akcay03 <mustafa.akcay@thomasmore.be>
2026-04-29 17:57:42 +0200

    Adjusted padel day repository

--- src/main/java/be/thomasmore/padelplanning/repositories/PadelDayRepository.java
+++ src/main/java/be/thomasmore/padelplanning/repositories/PadelDayRepository.java
@@ -1,7 +1,10 @@
 package be.thomasmore.padelplanning.repositories;
 
 import be.thomasmore.padelplanning.model.PadelDay;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PadelDayRepository extends CrudRepository<PadelDay, Integer> {
+    @Query("SELECT pd FROM PadelDay pd ORDER BY pd.date DESC LIMIT 1")
+    PadelDay getLast();
 }
