c3bf548a8563702c8493f935da747dd4ac4893f9
Justin Van Beek <justinvnbk@gmail.com>
2026-05-01 13:35:30 +0200

    When a PadelPlan is made, /admin/plan now shows the plan with dropdowns, each team can be edited and saved individually

--- src/main/java/be/thomasmore/padelplanning/controllers/admin/PlanController.java
+++ src/main/java/be/thomasmore/padelplanning/controllers/admin/PlanController.java
@@ -2,18 +2,18 @@ package be.thomasmore.padelplanning.controllers.admin;
 
 import be.thomasmore.padelplanning.model.Field;
 import be.thomasmore.padelplanning.model.PadelDay;
+import be.thomasmore.padelplanning.model.Player;
+import be.thomasmore.padelplanning.model.Team;
 import be.thomasmore.padelplanning.repositories.FieldRepository;
 import be.thomasmore.padelplanning.repositories.PadelDayRepository;
 import be.thomasmore.padelplanning.repositories.PlayerRepository;
+import be.thomasmore.padelplanning.repositories.TeamRepository;
 import be.thomasmore.padelplanning.services.CreatePadelDayService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -28,30 +28,49 @@ public class PlanController {
     private final CreatePadelDayService createPadelDayService;
     private final PadelDayRepository padelDayRepository;
     private final Logger logger= LoggerFactory.getLogger(this.getClass());
+    private final TeamRepository teamRepository;
 
-    public PlanController(FieldRepository fieldRepository, PlayerRepository playerRepository, CreatePadelDayService createPadelDayService, PadelDayRepository padelDayRepository) {
+    public PlanController(FieldRepository fieldRepository, PlayerRepository playerRepository, CreatePadelDayService createPadelDayService, PadelDayRepository padelDayRepository, TeamRepository teamRepository) {
         this.fieldRepository = fieldRepository;
         this.playerRepository = playerRepository;
         this.createPadelDayService = createPadelDayService;
         this.padelDayRepository = padelDayRepository;
+        this.teamRepository = teamRepository;
     }
 
     @GetMapping("/plan")
     public String plan(Model model){
         Optional<PadelDay> optionalPadelDay = padelDayRepository.getLast();
+        boolean hasPlan = false;
         if(optionalPadelDay.isPresent()){
             model.addAttribute("padelDay", optionalPadelDay.get());
+            hasPlan = !optionalPadelDay.get().getMatches().isEmpty();
         }
-
+        model.addAttribute("hasPlan", hasPlan);
         return "admin/plan";
     }
+
+    //Create a new plan for the curren PadelDay
     @PostMapping("/plan")
     public String postPlan(Model model,@RequestParam int id){
         PadelDay padelDay = padelDayRepository.findById(id).get();
-        padelDay.setFields(fieldRepository.getAvailable());
-        padelDay.setSignedUpPlayers(playerRepository.getAll());
-        padelDayRepository.save(padelDay);
         createPadelDayService.newPadelDayPlanning(padelDay);
+        return "redirect:/admin/plan";
+    }
+
+
+    @PostMapping("/planEdit")
+    public String postPlanEdit(@ModelAttribute Team team, @RequestParam List<Integer> playerIds){
+        Optional<Player> optionalPlayer1 = playerRepository.findById(playerIds.get(0));
+        Optional<Player> optionalPlayer2 = playerRepository.findById(playerIds.get(1));
+        if(optionalPlayer1.isPresent() && optionalPlayer2.isPresent()){
+            Player player1 = optionalPlayer1.get();
+            Player player2 = optionalPlayer2.get();
+
+            team.setPlayers(List.of(player1, player2));
+            teamRepository.save(team);
+        }
+
         return "redirect:/admin/plan";
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -46,5 +46,5 @@ VALUES
 -- VALUES
 -- ('2026-04-25 18:00:00', 9);
 
- INSERT INTO padel_day (date, number_of_matches)
- VALUES ('2026-04-25 18:00:00', 3);
+--  INSERT INTO padel_day (date, number_of_matches)
+--  VALUES ('2026-04-25 18:00:00', 3);
