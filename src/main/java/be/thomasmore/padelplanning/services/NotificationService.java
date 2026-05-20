d5e43aa243cf9c12070019c39f23119cd7ad25b7
Justin Van Beek <justinvnbk@gmail.com>
2026-05-20 14:43:34 +0200

    Notifications are now send over mailtrap sandbox

--- src/main/java/be/thomasmore/padelplanning/services/NotificationService.java
+++ src/main/java/be/thomasmore/padelplanning/services/NotificationService.java
@@ -1,5 +1,12 @@
 package be.thomasmore.padelplanning.services;
 
+import io.mailtrap.client.MailtrapClient;
+import io.mailtrap.config.MailtrapConfig;
+import io.mailtrap.factory.MailtrapClientFactory;
+import io.mailtrap.model.request.emails.Address;
+import io.mailtrap.model.request.emails.MailtrapMail;
+
+import java.util.List;
 import be.thomasmore.padelplanning.model.Notification;
 import be.thomasmore.padelplanning.model.Player;
 import be.thomasmore.padelplanning.repositories.NotificationRepository;
@@ -9,12 +16,13 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 @Service
 public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final PlayerRepository playerRepository;
+    //Token for mailing
+    private static final String TOKEN = "5832c48314d3ccfec3b4115c6516b0a3";
 
     public NotificationService(NotificationRepository notificationRepository, PlayerRepository playerRepository) {
         this.notificationRepository = notificationRepository;
@@ -33,6 +41,33 @@ public class NotificationService {
         for (Player p : recipients) {
             p.setHasUnseenNotifications(true);
             playerRepository.save(p);
+        }
+
+        MailtrapJavaSDKTest(title,message,recipients);
+    }
+
+    private void MailtrapJavaSDKTest(String title, String message, Collection<Player> recipients) {
+        final MailtrapConfig config = new MailtrapConfig.Builder()
+                .sandbox(true)
+                .inboxId(4645421L)
+                .token(TOKEN)
+                .build();
+
+        final MailtrapClient client = MailtrapClientFactory.createMailtrapClient(config);
+
+        List<Address> addresses = recipients.stream().map(r -> new Address(r.getEmail())).toList();
+
+        final MailtrapMail mail = MailtrapMail.builder()
+                .from(new Address("sandbox@example.com", "Mailtrap Test"))
+                .to(addresses)
+                .subject(title)
+                .text(message)
+                .build();
+
+        try {
+            System.out.println(client.send(mail));
+        } catch (Exception e) {
+            System.out.println("Caught exception : " + e);
         }
     }
 }
