4896bca536c59c094eec847842aeca7cc2e935c5
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-05-31 23:26:54 +0200

    Betalingspagina gebruikt aparte spelerslijsten

--- src/main/java/be/thomasmore/padelplanning/controllers/admin/ConfirmPaymentController.java
+++ src/main/java/be/thomasmore/padelplanning/controllers/admin/ConfirmPaymentController.java
@@ -1,30 +1,23 @@
 package be.thomasmore.padelplanning.controllers.admin;
 
 import be.thomasmore.padelplanning.model.PadelDay;
-import be.thomasmore.padelplanning.model.Player;
 import be.thomasmore.padelplanning.repositories.PadelDayRepository;
-import be.thomasmore.padelplanning.repositories.PlayerRepository;
-import be.thomasmore.padelplanning.services.NotificationService;
+import be.thomasmore.padelplanning.services.PlayerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
 public class ConfirmPaymentController {
     private final PadelDayRepository padelDayRepository;
-    private final PlayerRepository playerRepository;
-    private final NotificationService notificationService;
+    private final PlayerService playerService;
 
-    public ConfirmPaymentController(PadelDayRepository padelDayRepository, PlayerRepository playerRepository, NotificationService notificationService) {
+    public ConfirmPaymentController(PadelDayRepository padelDayRepository, PlayerService playerService) {
         this.padelDayRepository = padelDayRepository;
-        this.playerRepository = playerRepository;
-        this.notificationService = notificationService;
+        this.playerService = playerService;
     }
 
     @GetMapping("/payments/{id}")
@@ -32,23 +25,15 @@ public class ConfirmPaymentController {
                            @PathVariable Integer id) {
         Optional<PadelDay> optionalPadelDay = padelDayRepository.findById(id);
 
+        if (optionalPadelDay.isEmpty()) return "redirect:/";
+
         if (optionalPadelDay.isPresent()) {
             PadelDay padelDay = optionalPadelDay.get();
 
-            List<Player> paidPlayers = padelDay.getSignedUpPlayers().stream()
-                    .filter(player -> player.getPayedPadelDays().contains(padelDay))
-                    .toList();
-
-            List<Player> unpaidPlayers = padelDay.getSignedUpPlayers().stream()
-                    .filter(player -> !player.getPayedPadelDays().contains(padelDay))
-                    .toList();
-
-            List<Player> reservedPlayers = padelDay.getReservedPlayers();
-
             model.addAttribute("padelDay", padelDay);
-            model.addAttribute("paidPlayers", paidPlayers);
-            model.addAttribute("unpaidPlayers", unpaidPlayers);
-            model.addAttribute("reservedPlayers", reservedPlayers);
+            model.addAttribute("paidPlayers", playerService.getPaidPlayers(padelDay));
+            model.addAttribute("unpaidPlayers", playerService.getUnpaidPlayers(padelDay));
+            model.addAttribute("reservedPlayers", padelDay.getReservedPlayers());
         }
 
         return "admin/payments";
