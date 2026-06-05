1bcadc9cb61bfdde042e3f696f2764d9bac77929
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-06-05 23:43:25 +0200

    check voor speler of geregistreerd is bij het evenement of niet en check of evenement vol is of niet

--- src/main/java/be/thomasmore/padelplanning/controllers/user/UserEventController.java
+++ src/main/java/be/thomasmore/padelplanning/controllers/user/UserEventController.java
@@ -1,13 +1,16 @@
 package be.thomasmore.padelplanning.controllers.user;
 
 import be.thomasmore.padelplanning.model.ClubEvent;
+import be.thomasmore.padelplanning.model.Player;
 import be.thomasmore.padelplanning.repositories.ClubEventRepository;
+import be.thomasmore.padelplanning.repositories.PlayerRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,9 +18,11 @@ import java.util.Optional;
 @RequestMapping("/user")
 public class UserEventController {
     private final ClubEventRepository clubEventRepository;
+    private final PlayerRepository playerRepository;
 
-    public UserEventController(ClubEventRepository clubEventRepository) {
+    public UserEventController(ClubEventRepository clubEventRepository, PlayerRepository playerRepository) {
         this.clubEventRepository = clubEventRepository;
+        this.playerRepository = playerRepository;
     }
 
     @GetMapping("/events")
@@ -30,14 +35,24 @@ public class UserEventController {
     }
 
     @GetMapping("/events/{eventId}")
-    public String eventDetails(@PathVariable Integer eventId, Model model) {
+    public String eventDetails(@PathVariable Integer eventId, Model model, Principal principal) {
         Optional<ClubEvent> optionalClubEvent = clubEventRepository.findById(eventId);
 
         if (optionalClubEvent.isEmpty() || !optionalClubEvent.get().isPublished()) {
             return "redirect:/user/events";
         }
 
-        model.addAttribute("clubEvent", optionalClubEvent.get());
+        ClubEvent clubEvent = optionalClubEvent.get();
+        Player loggedPlayer = playerRepository.findByEmail(principal.getName());
+
+        boolean isRegistered = clubEvent.getParticipants().contains(loggedPlayer);
+
+        boolean isFull = clubEvent.getMaximumParticipants() != null
+                && clubEvent.getParticipants().size() >= clubEvent.getMaximumParticipants();
+
+        model.addAttribute("clubEvent", clubEvent);
+        model.addAttribute("isRegistered", isRegistered);
+        model.addAttribute("isFull", isFull);
 
         return "user/eventdetails";
     }
