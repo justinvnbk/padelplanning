1a68ac8ca45165640592bcf181bcc59fc02caf94
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-06-05 23:25:38 +0200

    event details pagina voor users

--- src/main/java/be/thomasmore/padelplanning/controllers/user/UserEventController.java
+++ src/main/java/be/thomasmore/padelplanning/controllers/user/UserEventController.java
@@ -5,9 +5,11 @@ import be.thomasmore.padelplanning.repositories.ClubEventRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
@@ -25,5 +27,18 @@ public class UserEventController {
         model.addAttribute("clubEvents", clubEvents);
 
         return "user/events";
+    }
+
+    @GetMapping("/events/{eventId}")
+    public String eventDetails(@PathVariable Integer eventId, Model model) {
+        Optional<ClubEvent> optionalClubEvent = clubEventRepository.findById(eventId);
+
+        if (optionalClubEvent.isEmpty() || !optionalClubEvent.get().isPublished()) {
+            return "redirect:/user/events";
+        }
+
+        model.addAttribute("clubEvent", optionalClubEvent.get());
+
+        return "user/eventdetails";
     }
 }
