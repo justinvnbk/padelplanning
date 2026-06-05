66350098c9d333bf2335ee417dcc737a89cad67d
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-06-05 21:59:19 +0200

    ClubEventController voor users gemaakt

---
+++ src/main/java/be/thomasmore/padelplanning/controllers/user/ClubEventController.java
@@ -0,0 +1,29 @@
+package be.thomasmore.padelplanning.controllers.user;
+
+import be.thomasmore.padelplanning.model.ClubEvent;
+import be.thomasmore.padelplanning.repositories.ClubEventRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class ClubEventController {
+    private final ClubEventRepository clubEventRepository;
+
+    public ClubEventController(ClubEventRepository clubEventRepository) {
+        this.clubEventRepository = clubEventRepository;
+    }
+
+    @GetMapping("/events")
+    public String events (Model model) {
+        List<ClubEvent> clubEvents = clubEventRepository.findUpcomingPublishedEvents();
+
+        model.addAttribute("clubEvents", clubEvents);
+
+        return "user/events";
+    }
+}
